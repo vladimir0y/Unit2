11a01a4b7b8e9777bd347f9cdd6113ca3b4a8710
vladimir0y <32306846+vladimir0y@users.noreply.github.com>
2023-02-23 12:09:28 +0200

    Add files via upload

--- story_content/user.js
+++ story_content/user.js
@@ -2,20 +2,50 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "6aAdN3KHZsv":
+      case "6Ui10qNzfnv":
         Script1();
         break;
-      case "5m9RBhXRGle":
+      case "69rKeXYiZ8R":
         Script2();
         break;
-      case "6aSc7PN1rpR":
+      case "5UxFJEO0YUJ":
         Script3();
         break;
-      case "5k904q45dqy":
+      case "6V2mczYdxBT":
         Script4();
         break;
-      case "69pTYChLtcY":
+      case "6pjgVthBxnL":
         Script5();
+        break;
+      case "6HUGTh78iIB":
+        Script6();
+        break;
+      case "6UQ4HRbKDO9":
+        Script7();
+        break;
+      case "61FRvTwYoMB":
+        Script8();
+        break;
+      case "5iC7yFMfu1b":
+        Script9();
+        break;
+      case "6JXQBDOMqpD":
+        Script10();
+        break;
+      case "5w9gexyZVEg":
+        Script11();
+        break;
+      case "63e4pIv0wQi":
+        Script12();
+        break;
+      case "5Zdb6ZJD6cD":
+        Script13();
+        break;
+      case "5ZdnIJOjjrg":
+        Script14();
+        break;
+      case "6O3iPZy10bm":
+        Script15();
         break;
   }
 }
@@ -41,6 +71,56 @@ function Script4()
 }
 
 function Script5()
+{
+  clearRecording()
+}
+
+function Script6()
+{
+  stopRecording()
+}
+
+function Script7()
+{
+  startRecording();
+}
+
+function Script8()
+{
+  playRecording()
+}
+
+function Script9()
+{
+  stopPlayback()
+}
+
+function Script10()
+{
+  clearRecording()
+}
+
+function Script11()
+{
+  stopRecording()
+}
+
+function Script12()
+{
+  startRecording();
+}
+
+function Script13()
+{
+  playRecording()
+}
+
+function Script14()
+{
+  stopPlayback()
+}
+
+function Script15()
 {
   clearRecording()
 }
